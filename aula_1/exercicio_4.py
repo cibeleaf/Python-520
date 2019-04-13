28c690f1ae58cb42033f655668ae028b87a80e72
Diogo Moura <diogomoura167@gmail.com>
2019-04-13 13:17:45 -0300

    aula 2.5

---
+++ aula_1/exercicio_4.py
@@ -0,0 +1,171 @@
+lista_de_usuarios = [
+  {
+    "nome": "qMHHOwoAaYECgB",
+    "idade": 49,
+    "email": "yMEpqGsegWXw@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua NIbCtFYJpZtRJg"
+  },
+  {
+    "nome": "GJBxVjGYtVEqmRasx",
+    "idade": 23,
+    "email": "mBZWAcaUDCVioOQEdIu@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua VFJGPBuNRnz"
+  },
+  {
+    "nome": "TFeiNzjMuAfyYHksx",
+    "idade": 39,
+    "email": "BEjDyrdxPJoniHIDFgZ@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua AEhMBiFAuGgSAiuIyx"
+  },
+  {
+    "nome": "EguHnoFTrUAuUUoery",
+    "idade": 37,
+    "email": "QzqgJRboorIzKjKp@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua LgktJSYDjo"
+  },
+  {
+    "nome": "zDjQIfYyTwtzetXMJvDs",
+    "idade": 25,
+    "email": "CZBArUyyMa@4linux.com",
+    "sexo": "F",
+    "endereco": "Rua DEPwqBeLgyylX"
+  },
+  {
+    "nome": "FDLtOesbqzdqzGe",
+    "idade": 47,
+    "email": "MakWfrgesmuBb@4linux.com",
+    "sexo": "F",
+    "endereco": "Rua qVLKGbVYOPRNTdrRYA"
+  },
+  {
+    "nome": "xrffFndgNldHhb",
+    "idade": 20,
+    "email": "MUvdkHayaYULCTkSDH@4linux.com",
+    "sexo": "F",
+    "endereco": "Rua ZwkeJCYdeZgEcX"
+  },
+  {
+    "nome": "RyfOocoNhyuEDSN",
+    "idade": 50,
+    "email": "ggQNSQvIOmDqZj@4linux.com",
+    "sexo": "M",
+    "endereco": "Rua kvvnDojQEMsHMXzc"
+  },
+  {
+    "nome": "GAXCbICkOjzNZxU",
+    "idade": 31,
+    "email": "ClNJyJMhNfmTvyQxxYvQ@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua yIParNvhlayIcGup"
+  },
+  {
+    "nome": "wvaTNhfqPOlnt",
+    "idade": 29,
+    "email": "EwTJHbRTCrfYF@4linux.com",
+    "sexo": "F",
+    "endereco": "Rua bGCVdBFXSzcVBeLvKYUZ"
+  },
+  {
+    "nome": "qhDBAYlbwaSMar",
+    "idade": 27,
+    "email": "AwDWEkwtZcGgaHlsaW@4linux.com",
+    "sexo": "M",
+    "endereco": "Rua hVYSkEHTmaBIJG"
+  },
+  {
+    "nome": "fAZzfzvEySnSei",
+    "idade": 44,
+    "email": "wrzgKwORTAnliywEq@4linux.com",
+    "sexo": "M",
+    "endereco": "Rua cSdMqmMvebnTd"
+  },
+  {
+    "nome": "HJBLhUuMHeAogrtbeq",
+    "idade": 47,
+    "email": "akKkICosKJbUdVcLTCnQ@4linux.com",
+    "sexo": "F",
+    "endereco": "Rua IhyoOwKTUCQlBvDuVVYS"
+  },
+  {
+    "nome": "vEeSluRuOmIJTFrgNA",
+    "idade": 35,
+    "email": "wrCGKAsQRlWLKUSH@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua ZRfcRxPsJPO"
+  },
+  {
+    "nome": "OyehQbxCsnSGPBLcviBs",
+    "idade": 23,
+    "email": "fnndYbeFLPmhB@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua cnquXCCUji"
+  },
+  {
+    "nome": "JDZdCgzulyGWW",
+    "idade": 30,
+    "email": "brPbOrQBWfIBikDA@4linux.com",
+    "sexo": "M",
+    "endereco": "Rua HgtuwqKeMin"
+  },
+  {
+    "nome": "MoinSGxEySEgrb",
+    "idade": 36,
+    "email": "qolygpBXCKEeur@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua xoVGMdTrFKHboQRzFd"
+  },
+  {
+    "nome": "XStrcVVPyieJJznYZ",
+    "idade": 49,
+    "email": "doKMuRYdKwolzgkq@4linux.com",
+    "sexo": "M",
+    "endereco": "Rua jeTpYoJAHnSeWAUWG"
+  },
+  {
+    "nome": "uWVZbFSfdE",
+    "idade": 30,
+    "email": "WpRAukMAAygh@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua QQvsljaHXnhqjHlg"
+  },
+  {
+    "nome": "jgVjBVFVVKui",
+    "idade": 43,
+    "email": "SCUVGmVBRpREfAMIleo@4linux.com",
+    "sexo": "?",
+    "endereco": "Rua ioMdZJPBgdMUzWsDg"
+  }
+]
+
+lista_de_usuarios_filtrados = []
+
+for usuario in lista_de_usuarios:
+    
+    cond_1 = 'j' in usuario['email'].lower()
+    cond_2 = 'l' in usuario['email'].lower()
+    cond_3 = usuario['idade'] > 25
+
+    if cond_1 or cond_2:
+        if cond_3:
+            lista_de_usuarios_filtrados.append(usuario)
+
+TEMPLATE = '{};{};{};{};{};'
+
+CABECALHO = TEMPLATE.format('NOME', 'IDADE', 'ENDEREÇO', 'SEXO', 'EMAIL')
+
+print(CABECALHO)
+for usuario in lista_de_usuarios_filtrados:
+    usuario_formatado = TEMPLATE.format(
+        usuario['nome'],
+        usuario['idade'],
+        usuario['endereco'],
+        usuario['sexo'],
+        usuario['email'],
+    )
+    print(usuario_formatado)
+    
+#Letras buscadas j OU l E idade > 25
